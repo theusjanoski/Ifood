313f435bcb1dae7bb817edf3bc436a69f6e01728
Matheus Janoski <matheusjanoski012@gmail.com>
2024-05-12 17:10:57 -0300

    verificãção input

--- np2/login/script_user.js
+++ np2/login/script_user.js
@@ -1,11 +1,19 @@
 function habilitarBotao() {
-  var nome = document.getElementById("nome");
-  var senha = document.getElementById("senha");
+  var nome = document.getElementById("nome").value.trim();
+  var senha = document.getElementById("senha").value.trim();
   var loginButton = document.getElementById("loginButton");
 
-  if (nome.value.trim() !== "" && senha.value.trim() !== "") {
-    loginButton.disabled = false;
+  if (nome !== "" && senha !== "") {
+      loginButton.disabled = false;
   } else {
-    loginButton.disabled = true;
+      loginButton.disabled = true;
   }
 }
+
+function fazerLogin() {
+      window.location.href = "/np2/inicio/index.html";
+}
+
+document.getElementById("nome").addEventListener("input", habilitarBotao);
+document.getElementById("senha").addEventListener("input", habilitarBotao);
+document.getElementById("loginButton").addEventListener("click", fazerLogin);
